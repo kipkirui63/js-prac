ce2715df82c25a66d44d0fabcbdd28fc6f951183
kipkirui63 <enocksang8356@gmail.com>
2023-09-14 13:12:08 +0300

    added to credit.js

---
+++ areaoftriangle.js
@@ -0,0 +1,19 @@
+// let width = parseInt(readline(),10);
+// let lenght = parseInt(readline(),10);
+
+// function area(x,y){
+//     return x*y;
+// }
+
+// let result = area(width, height);
+// console.log(result);
+
+// let width = parseInt(readline(),10);
+// let length= parseInt(readline(),10);
+
+function area(x,y){
+    return x*y;
+}
+
+let result = area(3, 5);
+console.log(result);
---
+++ return.js
@@ -0,0 +1,6 @@
+function add(x,y){
+    return x+y;
+}
+
+let result = add(6,5);
+console.log(result);
