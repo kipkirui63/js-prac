c1b73a60bd76923fdc70999fdf17b571a3120f34
kipkirui63 <enocksang8356@gmail.com>
2023-09-14 12:17:37 +0300

    added to credit.js

---
+++ credit.js
@@ -0,0 +1,16 @@
+function credit(purchase){
+    if(purchase >=1500){
+        console.log("Disount")
+    }else{
+        console.log("No discoutnt")
+    }
+}
+credit(1900)
+
+let balance = 360;
+
+for (let i =1; balance >= 50;i++){
+    console.log(balance);
+    balance -=50
+
+}
